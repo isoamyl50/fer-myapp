66806e6805d9778fc30662e579c0c1bf9d583b11
Hoang Nguyen <100697734+isoamyl50@users.noreply.github.com>
2024-05-16 10:51:26 +0700

    chore: Update component import in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App.js";
+import HelloWorld from "./components/HelloWorld.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<HelloWorld />);
